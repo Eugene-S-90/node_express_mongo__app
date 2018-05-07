3092b559c44209b974ab89aae2f0a115bcf67bda
EugeneSokolyansky <evgenys@123software.ru>
2018-05-07 17:32:08 +0300

    add logout

--- routes/users.js
+++ routes/users.js
@@ -80,5 +80,12 @@ router.post('/register', (req, res) => {
     }
 });
 
+// LOGOUT USER
+router.get('/logout',(req,res)=>{
+    req.logout();
+    req.flash('success_msg','You are logged out');
+    res.redirect('/users/login');
+})
+
 
 module.exports = router;
